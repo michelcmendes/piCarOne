8a232e3a4b74d0bd839c6684fcc7a9bb51b12f42
michelcmendes <michelcmendes@gmail.com>
2017-07-09 16:58:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,46 +4,47 @@ import sys
 import Tkinter as tk
 from sensor import distance
 
+
 def init():
     gpio.setmode(gpio.BOARD)
-    gpio.setup(7, gpio.OUT)
-    gpio.setup(11, gpio.OUT)
-    gpio.setup(13, gpio.OUT)
-    gpio.setup(15, gpio.OUT)
+    gpio.setup(16, gpio.OUT)
+    gpio.setup(18, gpio.OUT)
+    gpio.setup(22, gpio.OUT)
+    gpio.setup(24, gpio.OUT)
 
 def reverse(tf):
-    gpio.output(7, False)
-    gpio.output(11, True)
-    gpio.output(13, False)
-    gpio.output(15, True)
+    gpio.output(16, False)
+    gpio.output(18, True)
+    gpio.output(22, False)
+    gpio.output(24, True)
     time.sleep(tf)
-    
+
 def forward(tf):
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(13, True)
-    gpio.output(15, False)
+    gpio.output(16, True)
+    gpio.output(18, False)
+    gpio.output(22, True)
+    gpio.output(24, False)
     time.sleep(tf)
-    
+
 def turn_right(tf):
-    gpio.output(7, True)
-    gpio.output(11, False)
-    gpio.output(13, False)
-    gpio.output(15, True)
+    gpio.output(16, True)
+    gpio.output(18, False)
+    gpio.output(22, False)
+    gpio.output(24, True)
     time.sleep(tf)
-    
+
 def turn_left(tf):
-    gpio.output(7, False)
-    gpio.output(11, True)
-    gpio.output(13, True)
-    gpio.output(15, False)
+    gpio.output(16, False)
+    gpio.output(18, True)
+    gpio.output(22, True)
+    gpio.output(24, False)
     time.sleep(tf)
 
 def stop(tf):
-    gpio.output(7, False)
-    gpio.output(11, False)
-    gpio.output(13, False)
-    gpio.output(15, False)
+    gpio.output(16, False)
+    gpio.output(18, False)
+    gpio.output(22, False)
+    gpio.output(24, False)
     time.sleep(tf)
     gpio.cleanup()
 
@@ -52,7 +53,7 @@ def key_input(event):
     print "Key:", event.char
     key_press = event.char
     sleep_time = 0.060
-    
+
     if key_press.lower() == "w":
         forward(sleep_time)
     elif key_press.lower() == "s":
@@ -62,19 +63,17 @@ def key_input(event):
     elif key_press.lower() == "d":
         turn_right(sleep_time)
     elif key_press.lower() == "p":
-        stop(sleep_time) 
+        stop(sleep_time)
     else:
         pass
 
     curDis = distance("cm")
     print("Distance:", curDis)
-    
+
     if curDis <15:
         init()
         reverse(0.5)
 
-command = tk.TK()
+command = tk.Tk()
 command.bind('<keypress>', key_input)
 command.mainloop()
-
-    
