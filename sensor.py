ed2ce3be8ef4db777517517dc86d50bbebed99ad
michelcmendes <michelcmendes@gmail.com>
2017-07-09 16:58:57 +0100

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -3,32 +3,31 @@ import time
 
 def distance(measure='cm'):
     gpio.setmode(gpio.BOARD)
-    gpio.setup(12, gpio.OUT)
-    gpio.setup(16, gpio.IN)
-  
+    gpio.setup(10, gpio.OUT)
+    gpio.setup(12, gpio.IN)
+
     time.sleep(0.3)
-    gpio.ouput(12, True)
+    gpio.output(10, True)
     time.sleep(0.00001)
-    
-    gpio.output(12, False)
-    while gpio.input(16) == 0:
+
+    gpio.output(10, False)
+    while gpio.input(12) == 0:
         nosig = time.time()
-    
-    while gpio.input(16) == 1:
+
+    while gpio.input(12) == 1:
         sig = time.time()
-    
+
     tl = sig - nosig
-    
+
     if measure == 'cm':
         distance = tl / 0.000058
-    elif measure == 'in:
+    elif measure == 'in':
         distance = tl / 0.000148
     else:
         print('Improper choice of measurement: in or cm')
         distance = None
-    
+
     gpio.cleanup()
     return distance
-  
+
 print(distance('cm'))
-    
