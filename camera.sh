9b437b80cbca4671e8b3108a992c175e3d0ecb74
michelcmendes <michelcmendes@gmail.com>
2017-09-07 21:34:54 +0100

    camera stream update

--- camera.sh
+++ camera.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-raspivid -o - -t 0 -hf -w 600 -h 400 -fps 30 |cvlc -v stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8554}' :demux=h264
+#raspivid -o - -t 0 -hf -w 600 -h 400 -fps 30 |cvlc -v stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8554}' :demux=h264
+
+raspivid -o - -t 0 -n -w 600 -h 400 -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264
+
